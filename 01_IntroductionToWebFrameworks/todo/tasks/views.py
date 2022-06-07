b437133a4866effef95acebc6c5d66300715835a
@aleksandr_alekseev <name_sasha@mail.ru>
2022-06-07 19:24:03 +0300

    2.6 Практическая работа

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 
 from django.views import View
-
-
+import random
+random_elem = range(1, 100, 1)
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
@@ -11,4 +11,5 @@ class ToDoView(View):
                             '<li>Установить django</li>'
                             '<li>Запустить сервер</li>'
                             '<li>Порадоваться результату</li>'
-                            '</ul>')
+                            '<li>Что-то новенькое {rand}'
+                            '</ul>'.format(rand=random_elem[random.randint(1, 100)]))
