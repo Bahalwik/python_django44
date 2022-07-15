c5079b83764d041270e3e78f761b767617d26ae1
@aleksandr_alekseev <name_sasha@mail.ru>
2022-07-15 12:28:05 +0300

    test

--- 04_DatabasesAndModels/board/advertisements_app/apps.py
+++ 04_DatabasesAndModels/board/advertisements_app/apps.py
@@ -3,4 +3,3 @@ from django.apps import AppConfig
 
 class AdvertisementsConfig(AppConfig):
     name = 'advertisements_app'
-
--- 04_DatabasesAndModels/board/advertisements_app/urls.py
+++ 04_DatabasesAndModels/board/advertisements_app/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-
+    path('', views.home, name='home'),
+    # path('advertisements', views.advertisement_list, name='advertisement_list'),
+    # path('create_advertisement', views.create_advertisement, name='create_advertisement'),
+    # path('contacts', views.contacts, name='contacts'),
+    # path('about', views.about, name='about')
 ]
