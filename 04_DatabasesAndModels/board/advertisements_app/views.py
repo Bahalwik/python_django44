b69ed7829cec92a3b1548a0b9960d0abce918866
ALEKSANDR ALEKSEEV <32517590+Bahalwik@users.noreply.github.com>
2022-07-15 14:17:56 +0300

    Delete 04_DatabasesAndModels/board directory

--- 04_DatabasesAndModels/board/advertisements_app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import *
-
-
-def home(request):
-    products_images = ProductImg.objects.filter(is_active=True, is_main=True)
-    return render(request, 'advertisements_app/home.html', locals())
-
